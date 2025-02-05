605460d6e28ca96ceca39de77d252c97e7680e4c
Raúl Gonzalez <rgonzalef@gmail.com>
2025-02-04 20:51:23 -0500

    new deploy v2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rgonzalef.github.io/tu-repo', // Cambia esto con tu URL de GitHub Pages
+  base: '/astro-app/', // Reemplaza "tu-repo" con el nombre real de tu repositorio
+  output: 'static', // Asegura que Astro genere archivos estáticos
   vite: {
     plugins: [tailwindcss()],
   },
